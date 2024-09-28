1bcc6f7b73bff4ded84c739db56545ed3c431ff7
Craole <32288735+Craole@users.noreply.github.com>
2024-09-27 19:01:55 -0500

    modularizing

---
+++ home/configurations/craole/gui.nix
@@ -0,0 +1,15 @@
+{ editor, ... }:
+{
+  programs.git = {
+    enable = true;
+    userName = "Craole";
+    userEmail = "32288735+Craole@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+        inherit editor;
+      };
+      credentials.helper = "cache --timeout=3600";
+    };
+  };
+}
