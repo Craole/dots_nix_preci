2143310bf69df623fd81c8f5f4b6fc669e5bf76c
Craole <32288735+Craole@users.noreply.github.com>
2024-10-25 17:08:38 -0500

    Auto Update

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -1,3 +1,18 @@
 {
-  git = import ./git;
+  # anyrun = import ./anyrun;
+  # atuin = import ./atuin;
+  bash = import ./bash;
+  bat = import ./bat;
+  beets = import ./beets;
+  bitwarden = import ./bitwarden;
+  brave = import ./brave;
+  broot = import ./broot;
+  chromium = import ./chromium;
+  cmus = import ./cmus;
+  comms = import ./comms;
+  curseradio = import ./curseradio;
+  direnv = import ./direnv;
+  doublecmd = import ./doublecmd;
+  downloaders = import ./downloaders;
+
 }
