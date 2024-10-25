d8d3a650cc308570bdd4f4290c1770f524d5d07e
Craole <32288735+Craole@users.noreply.github.com>
2024-10-25 16:44:33 -0500

    Auto Update

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -1,12 +1,3 @@
 {
-  pkgs ? import <nixpkgs> { },
-}:
-
-let
-  # Import all directories in the current directory (home/packages)
-  packages = builtins.mapAttrs (name: path: import (path + "/default.nix")) (
-    builtins.attrNames (builtins.readDir ./.)
-  );
-in
-# Expose all packages as an attribute set
-packages
+  git = import ./git.nix;
+}
